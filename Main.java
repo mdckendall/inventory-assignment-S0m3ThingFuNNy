57efb8e5d8a4eac208e825b5cfd66090e4366c37
CommandoBoss <78758224+S0m3ThingFuNNy@users.noreply.github.com>
2023-03-28 02:06:42 +0000

    removed a singular space bar (if this works something will get hurt)

--- Main.java
+++ Main.java
@@ -72,7 +72,7 @@ class Main {
           for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
             if (i != arr.length - 1) {
-              System.out.print(", ");
+              System.out.print(",");
             }
           }
           System.out.println();
